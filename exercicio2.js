e694ba947896c1355091d33203a469f0250920f5
William Sallum <sallum.cefetmg@gmail.com>
2025-09-24 13:42:38 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -4,31 +4,31 @@
 const servidorDasImagens = 'https://willsallum.github.io/cefet_front_end/images';
 const imagens = [
     {
-      arquivo: '01-modelo-artistico-da-sonda.webp',
+      arquivo: '01-philae-parts.jpg',
       descricao: 'Imagem em 3D mostrando a sonda tocando um asteróide. ' +
         'A sonda tem forma de caixa com dois painéis solares em suas ' +
         'laterais e, na parte de baixo, um pequeno braço sai e encosta ' +
         'no asteróide'
     },
     {
-      arquivo: '02-lancamento-atlas-v-em-2017.webp',
+      arquivo: '02-philae-rosetta.jpg',
       descricao: 'Foto de foguete na vertical no momento de seu ' +
         'lançamento. Ele possui bandeiras da NASA, dos EUA e da empresa ' +
         'ULA e está com o motor sendo ligado, saindo fogo e fumaça'
     },
     {
-      arquivo: '03-distanciamento-da-sonda-da-terra.webp',
+      arquivo: '03-philae-separation.jpg',
       descricao: 'Foto do espaço quase toda preta mostrando a Terra bem ' +
         'pequena no canto esquerdo e, no canto direito, a lua ainda menor'
     },
     {
-      arquivo: '04-aproximacao-da-sonda-ao-bennu-em-2018.webp',
+      arquivo: '04-philae-67-picture.jpg',
       descricao: 'Foto do espaço mostrando o asteróide Bennu que tem ' +
         'a forma de um paralelepípedo com base losangular aproximadamente ' +
         'e a superfície acinzentada'
     },
     {
-      arquivo: '05-mapeamento-do-asteroide.webp',
+      arquivo: '05-philae-collecting.jpg',
       descricao: 'Animação de uma forma tridimensional um pouco ovalada ' +
         'representada usando diversas cores e girando em torno do eixo vertical ' +
         'representando a superfície do asteróide'
@@ -39,18 +39,6 @@ const imagens = [
         'asteróide Bennu mostrando uma pequena clareira em um ' +
         'terreno bem acidentado, com muitas pedras. No centro há ' +
         'um desenho no formato da sonda'
-    },
-    {
-      arquivo: '07-touch-and-go-e-detritos-suspensos-em-2020.webp',
-      descricao: 'Foto do asteróide Bennu com vários regolitos (pedras) ' +
-        'sendo suspensos após a sonda ter encostado para coletar amostras ' +
-        'da sua superfície'
-    },
-    {
-      arquivo: '08-sonda-distanciando-e-voltando-para-terra.webp',
-      descricao: 'Foto do asteróide Bennu com ele retratado em ' +
-        'menos da metade e com baixa iluminação, mostrando sua superfície ' +
-        'acinzentada'
     }
   ];
 
